9cf9cab3a2ef4aeeaeb831ccea7e95d1eb94f2a2
Amna Saleem <saleemamna431@gmail.com>
2024-10-14 17:22:15 -0700

    first commit

---
+++ stop watch/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace stop_watch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hrs = new System.Windows.Forms.Label();
+            this.mins = new System.Windows.Forms.Label();
+            this.sec = new System.Windows.Forms.Label();
+            this.minsec = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hrs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(249, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 45);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mins";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(393, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 45);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sec";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(524, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 45);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Minsec";
+            // 
+            // hrs
+            // 
+            this.hrs.AutoSize = true;
+            this.hrs.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hrs.Location = new System.Drawing.Point(140, 180);
+            this.hrs.Name = "hrs";
+            this.hrs.Size = new System.Drawing.Size(47, 35);
+            this.hrs.TabIndex = 4;
+            this.hrs.Text = "00";
+            this.hrs.Click += new System.EventHandler(this.hrs_Click);
+            // 
+            // mins
+            // 
+            this.mins.AutoSize = true;
+            this.mins.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mins.Location = new System.Drawing.Point(270, 180);
+            this.mins.Name = "mins";
+            this.mins.Size = new System.Drawing.Size(47, 35);
+            this.mins.TabIndex = 5;
+            this.mins.Text = "00";
+            // 
+            // sec
+            // 
+            this.sec.AutoSize = true;
+            this.sec.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sec.Location = new System.Drawing.Point(406, 180);
+            this.sec.Name = "sec";
+            this.sec.Size = new System.Drawing.Size(47, 35);
+            this.sec.TabIndex = 6;
+            this.sec.Text = "00";
+            // 
+            // minsec
+            // 
+            this.minsec.AutoSize = true;
+            this.minsec.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minsec.Location = new System.Drawing.Point(560, 180);
+            this.minsec.Name = "minsec";
+            this.minsec.Size = new System.Drawing.Size(47, 35);
+            this.minsec.TabIndex = 7;
+            this.minsec.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(118, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 48);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(276, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 48);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(412, 260);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 48);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "resume";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(566, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 48);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "restart";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.minsec);
+            this.Controls.Add(this.sec);
+            this.Controls.Add(this.mins);
+            this.Controls.Add(this.hrs);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label hrs;
+        private System.Windows.Forms.Label mins;
+        private System.Windows.Forms.Label sec;
+        private System.Windows.Forms.Label minsec;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ stop watch/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stop_watch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hrs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int msec = Convert.ToInt32(minsec.Text);
+            int secs = Convert.ToInt32(sec.Text);
+            int min = Convert.ToInt32(mins.Text);
+            int hr = Convert.ToInt32(hrs.Text);
+
+            msec = msec + 1;
+            minsec.Text = msec.ToString();
+            if (msec >= 60)
+            {
+                minsec.Text = "00";
+                secs = secs + 1;
+                sec.Text = secs.ToString();
+
+            }
+
+            else if (secs >= 60)
+            {
+                sec.Text = "00";
+                min = min + 1;
+                mins.Text = mins.ToString();
+
+            }
+            else if (min >= 60)
+            {
+                mins.Text = "00";
+                hr = hr + 1;
+                hrs.Text = hrs.ToString();
+
+
+
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            minsec.Text = "00";
+            sec.Text = "00";
+            mins.Text = "00";
+            hrs.Text = "00";
+            timer1.Enabled = false;
+        }
+    }
+}
